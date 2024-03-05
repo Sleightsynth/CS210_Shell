f95bba05e2d8f4b2ddfb98e0f6319c121c745dd3
Angus Fraser CS2022 <angus.fraser.2022@uni.strath.ac.uk>
2024-03-05 19:07:53 +0000

    Upload New File

---
+++ shellHeader.h
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <libgen.h> // for cd Stage 4
+#include <ctype.h> //used for stage 5 to check if character is a letter or number
+#define HISTORY_SIZE 20
+
+typedef struct aliasEntry {
+    char *alias;
+    char *command;
+    struct aliasEntry *ptr;
+}aliasEntry; // Struct for Alias
+
+aliasEntry aliasList[10];
+int aliases;
+
+void setHomeDirectory();
+char* getUserPath();
+void printPrompt();
+int internalCommands(char* tokenArray[], int i, char* history[], int historyCount);
+int externalCommands(char* tokenArray[], int i);
+void restorePath(char* path);
+void getPath();
+void setPath(char* new_path);
+void changeDirectory(const char *path);
+void showHistory(char* history[], int count);
+char* addToHistory(char* command, char* history[], int* count);
+char* invokeHistory(char* history[], char* command, int count);
+void saveHistory(char* history[], int count);
+void loadHistory(char* history[], int* count);
+int updateAlias (char *command, aliasEntry aliasList[], int count);
+int isAlias(char *string, aliasEntry aliasList[], int value, int count);
+void showAliases(aliasEntry aliasList[], int count);
+int addAlias(char *newAlias, char *command, aliasEntry aliasList[], int count);
+int removeAlias (char *aliasToRemove, aliasEntry aliasList[], int *count);
