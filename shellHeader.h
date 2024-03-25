0cd89d40cec8ecc8e24dfa562f423ccea8d4a29b
Stuart Gordon <stuart.gordon.2022@uni.strath.ac.uk>
2024-03-25 15:59:39 +0000

    added comments and spacing to shellHeader.h

--- shellHeader.h
+++ shellHeader.h
@@ -3,16 +3,17 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
-#include <libgen.h> // for cd Stage 4
-#include <ctype.h> //used for stage 5 to check if character is a letter or number
+#include <libgen.h>
+#include <ctype.h>
 #define HISTORY_SIZE 20
 
 typedef struct aliasEntry {
     char *alias;
     char *command;
     struct aliasEntry *ptr;
-} aliasEntry; // Struct for Alias
+} aliasEntry; //struct for Alias
 
+//shell.c
 void setHomeDirectory();
 char* getUserPath();
 void printPrompt();
@@ -22,11 +23,15 @@ void restorePath(char* path);
 void getPath();
 void setPath(char* new_path);
 void changeDirectory(const char *path);
+
+//shellHistory.c
 void showHistory(char* history[], int count);
 char* addToHistory(char* command, char* history[], int* count);
 char* invokeHistory(char* history[], char* command, int count);
 void saveHistory(char* history[], int count);
 void loadHistory(char* history[], int* count);
+
+//shellAlias.c
 int updateAlias (char *command, aliasEntry aliasList[], int count);
 int isAlias(char *string, aliasEntry aliasList[], int value, int count);
 void showAliases(aliasEntry aliasList[], int count);
